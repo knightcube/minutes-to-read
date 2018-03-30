90a7d786304e7fa0b8c0296e152b54bf2f5227bf
Rajat Kumar Gupta <rajatcube@gmail.com>
2018-03-30 12:29:37 +0530

    Library Added

--- app/src/main/java/com/example/mycoollibrary/minutestoreadapp/MainActivity.java
+++ app/src/main/java/com/example/mycoollibrary/minutestoreadapp/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.mycoollibrary.minutestoreadapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
